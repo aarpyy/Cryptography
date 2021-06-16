3f06a3967b8a3c255209750f954abb9e6a94daa2
aarpyy <acarpent@oberlin.edu>
2021-06-16 11:56:04 -0400

    Update ps3.2.py

--- ps3.2.py
+++ ps3.2.py
@@ -8,56 +8,64 @@ def StringToNum(s):
         result += (128**i) * ord(n[i])
     return result
 
+
 def PowerMod(x, y, m):
     result = x
     for _ in range(y-1):
         result = (result * x) % m
     return result
 
-def NumToString(n, p):
+
+def NumToString(n):
     s = ''
-    while n > 0:
+    while True:
         index = 0
         while True:
-            if n >= (128 ** index):
+            if (128 ** index) < n:
                 index += 1
             else:
                 index -= 1
                 break
         k = 128 ** index
-        print(n, k, index)
-        char = (n % k)
-        n -= k * char
-        s += chr(char)
-    return s
+        m = n // k
+        char = m * k
+        n -= char
+        s += chr(m)
+        if n == 0:
+            return s
+
 
 def Inverse(x, m):
     for i in range(m):
         if (x * i) % m == 1:
             return i
 
+
 p = 7954763
 g = 4
 a = 1234092
-# A = PowerMod(g, a, p)
-#
-# k = 6234043
-# m = StringToNum("Hi!")
-# c1 = PowerMod(g, k, p)
-# c2 = (m * PowerMod(A, k, p)) % p
+A = PowerMod(g, a, p)
 
-A = 1575862
-# c1 = 5138015
-# c2 = 3284434
+k = 6234043
+ma = StringToNum("Hi!")
+c1a = PowerMod(g, k, p)
+c2a = (ma * PowerMod(A, k, p)) % p
 
-c1 = 7724479
-c2 = 3951884
+print(f"\nProblem 2. a)\n"
+      f"-------------\n"
+      f"Alice publishes:\n"
+      f"A: {A}\n\n"
+      f"Bobs sends:\n"
+      f"c1: {c1a}\n"
+      f"c2: {c2a}\n")
 
-print(f"A: {A}\n"
-      f"c1: {c1}\n"
-      f"c2: {c2}")
+c1b = 7724479
+c2b = 3951884
 
+c1bi = Inverse(c1b, p)
+mb = (PowerMod(c1bi, a, p) * c2b) % p
 
-c1i = Inverse(c1, p)
-m = (PowerMod(c1i, a, p) * c2) % p
-print(NumToString(1193121, p))
+print(f"\nProblem 2. b)\n"
+      f"-------------\n"
+      f"Bobs' message:\n"
+      f"m: {NumToString(mb)}\n")
