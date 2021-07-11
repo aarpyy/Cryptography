7e959f7350a0e2ddf2ccac993ac5aa4ff0769fc2
aarpyy <acarpent@oberlin.edu>
2021-07-11 14:41:25 -0400

    bh

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 from crypto_functions import *
 
-# start1 = time.time()
-# p = RandomPrime(pow(2, 1000))
-# print(p)
-# print(f"K = 40 took: {time.time() - start1:.2f}s")
+# p = 822171430891193443624252802027
+# q = 411085715445596721812126401013
+# g = 3
+# a = 815739646779227
+# A = pow(g, a, p)
+# D = StringToNum("This is my sample document D.")
+# k = random.randrange(pow(2, 10))
+# S1 = pow(g, k, p) % q
+# S2 = ((D + a*S1) * pow(k, -1, q)) % q
+#
+# print(DSA(D, S1, S2, g, p, q, A))
 
-
-# print(MillerRabinPrimality(53))
-# print(Jacobi(19, 53))
-p = RandomPrime(pow(2, 1000))
-print(p)
-print(BailliePSW_Primality(p))
 
