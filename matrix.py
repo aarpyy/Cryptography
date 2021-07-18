ecf7982aca4aefffee6aacd43cfa0236bf9459fe
aarpyy <acarpent@oberlin.edu>
2021-07-18 11:50:26 -0400

    matrix

--- matrix.py
+++ matrix.py
@@ -20,6 +20,16 @@ def MakeMatrix(rows, cols):
     return numpy.array(mat)
 
 
+def CopyMatrix(matrix):
+    new_mat = []
+    for i in range(len(matrix)):
+        new_mat.append([])
+        for j in range(len(matrix[0])):
+            new_mat[i].append(matrix[i][j])
+
+    return numpy.array(new_mat)
+
+
 def MultiplyMatrix(matrixA=None, matrixB=None):
     if matrixA is None or matrixB is None:
         print("Enter dimensions for Matrix A: ")
@@ -53,11 +63,13 @@ def InvertMatrix(matrix=None):
     if matrix is None:
         size = int(input("Enter matrix dimensions nxn: "))
         matrix = MakeMatrix(size, size)
+    return numpy.linalg.inv(CopyMatrix(matrix))
 
-    return numpy.linalg.inv(matrix)
 
-
-def MatrixFloat(matrix):
+def MatrixFloat(matrix, bits=16):
+    float_types = {16: numpy.float16, 32: numpy.float32, 64: numpy.float64}
+    if bits in float_types:
+        return matrix.astype(float_types[bits])
     return matrix.astype(float)
 
 
@@ -85,11 +97,12 @@ def SortRREF(matrix):
         for j in range(len(matrix)):
             if M[j][i] == 1:
                 index = j
-        if index != -1:
+        if index > pivot_index:
             M[index][:], M[pivot_index][:] = M[pivot_index][:], M[index][:]
+        if index != -1:
             pivot_index += 1
 
-    return numpy.array(M)
+    return MatrixFloat(numpy.array(M))
 
 
 def RREF(matrix=None):
@@ -99,25 +112,28 @@ def RREF(matrix=None):
         cols = int(input("Cols: "))
         matrix = MakeMatrix(rows, cols)
 
-    row_length = len(matrix[0])
-    col_length = len(matrix)
+    matrixM = CopyMatrix(matrix)
+
+    row_length = len(matrixM[0])
+    col_length = len(matrixM)
     pivot_rows = []
     for i in range(row_length):
         pivot = False
         for j in range(col_length):
-            if matrix[j][i] != 0 and j not in pivot_rows:
-                shift = 1 / matrix[j][i]
-                for k in range(row_length):
-                    matrix[j][k] *= shift
+            if matrixM[j][i] != 0 and j not in pivot_rows:
+                if matrixM[j][i] != 1:
+                    shift = 1 / matrixM[j][i]
+                    for k in range(row_length):
+                        matrixM[j][k] *= shift
                 pivot_rows.append(j)
                 pivot = True
 
             if pivot:
                 for k in range(col_length):
-                    if k != j and matrix[k][i] != 0:
-                        shift = matrix[k][i]
+                    if k != j and matrixM[k][i] != 0:
+                        shift = matrixM[k][i]
                         for l in range(row_length):
-                            matrix[k][l] -= shift * matrix[j][l]
+                            matrixM[k][l] -= shift * matrixM[j][l]
                 break
 
-    return SortRREF(matrix)
+    return SortRREF(matrixM)
