827338812962cf92938119fd036355b1b9d7d952
aarpyy <acarpent@oberlin.edu>
2021-07-18 22:16:18 -0400

    0.1.2
    not sure why readme not appearing

--- build/lib/cryptography318/__init__.py
+++ build/lib/cryptography318/__init__.py
@@ -4,12 +4,5 @@ from cryptography318.bailliepsw_helper import Jacobi
 from cryptography318.prime import IsPrime, MillerRabinPrimality, MillerRabin_base_a, RandomPrime, ConfirmPrime, NextPrime, BailliePSW_Primality
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __author__ = 'Andrew Carpenter'
-
-"""
-Cryptography library as a local site-package, allowing convenient importing of key
-cryptographic, linear algebra, and primality functions. Imports all prime, matrix, and 
-cryptography318 libraries, in addition to frequently used math, random, statistics, 
-time, and numpy libraries.
-"""
