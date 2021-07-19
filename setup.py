3f37d1d57f1e7845bed0f5a311129a39dc3e53b8
aarpyy <acarpent@oberlin.edu>
2021-07-18 21:28:45 -0400

    h

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='cryptography',
+    name='cryptography318',
     version='0.1.0',
     description='A set of functions useful in cryptography',
     url='https://github.com/aarpyy/Cryptography',
     author='Andrew Carpenter',
     author_email='acarpent@oberlin.edu',
     license='BSD 2-clause',
-    packages=['cryptography'],
+    packages=['cryptography318'],
     install_requires=['mpi4py>=2.0',
                       'numpy',
                       ],
 
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Intended Audience :: Student',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
