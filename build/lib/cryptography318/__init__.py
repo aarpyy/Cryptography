02bfaae501bc352b01f41d668649d867716ac2f1
aarpyy <acarpent@oberlin.edu>
2021-07-20 13:52:02 -0400

    0.1.4
    added previous prime function, slightly updated text descriptions

--- build/lib/cryptography318/__init__.py
+++ build/lib/cryptography318/__init__.py
@@ -2,7 +2,8 @@ from cryptography318.crypto_functions import *
 from cryptography318.matrix import MakeMatrix, MultiplyMatrix, SquareMatrix, InvertMatrix, MatrixFloat, RREF
 from cryptography318.bailliepsw_helper import Jacobi
 from cryptography318.prime import IsPrime, MillerRabinPrimality, MillerRabin_base_a, RandomPrime, ConfirmPrime, NextPrime, BailliePSW_Primality
+import setuptools.version
 
 
-__version__ = "0.1.2"
+__version__ = setuptools.version.__version__
 __author__ = 'Andrew Carpenter'
--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='cryptography318',
-    version='0.1.3',
-    description='A set of functions useful in cryptography',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.1.4',
+    description='A set of functions useful in cryptography.',
     url='https://github.com/aarpyy/Cryptography',
     author='Andrew Carpenter',
     author_email='acarpent@oberlin.edu',
     packages=['cryptography318'],
-    install_requires=['numpy',
-                      ],
-
+    install_requires=['numpy'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Programming Language :: Python :: 3.8',
