d319505aa5f669e13f2619f9ba0c02fa1f5e2dd6
aarpyy <acarpent@oberlin.edu>
2023-12-25 13:40:24 -0500

    0.5.3 - bugfix

--- cryptography318/prime/__init__.py
+++ cryptography318/prime/__init__.py
@@ -1,7 +1,7 @@
 from .prime import (baillie_psw, chinese_remainder, confirm_prime, isprime, lift_sqrt, miller_rabin, next_prime,
-                    prev_prime, prime_range, quadratic_non_residue, quadratic_residue, randprime, sqrt_mod, get_details)
+                    prev_prime, prime_range, quadratic_non_residue, quadratic_residue, randprime, sqrt_mod)
 
 __all__ = [
     "randprime", "prime_range", "next_prime", "prev_prime", "isprime", "miller_rabin", "baillie_psw", "confirm_prime",
-    "sqrt_mod", "quadratic_residue", "quadratic_non_residue", "chinese_remainder", "lift_sqrt", "get_details"
+    "sqrt_mod", "quadratic_residue", "quadratic_non_residue", "chinese_remainder", "lift_sqrt"
 ]
